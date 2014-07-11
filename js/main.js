67a3ac5a0e844d0254f0334222a1afe8ee62a911
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-07-11 17:54:09 +0200

    separacion de template de vista logica de negocio y datos no persistentes en link externo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,50 @@
-//asignamos un nimbre a nuestro modelo para crear controladores de aplicacion
 (function(w, ng){
 	var app = ng.module('accountsApp', []);
 
-	app.controller('paramAccounts', function($scope, $filter){
-		//NOTA: utilizando $scope, NO se le puede dar alias al controlador en la vista; se llaman directamente a las propiedades por su nombre
+	//CONTROLADOR DE PROPIEDADES DE APLICACION (datos)
+	app.controller('bankAccountsController', function($scope, $filter){
+		this.titulo = 'Controlar el Cash Flow con - AngularJS';
 		
-		this.today = new Date().toJSON().split('T')[0];
-		//$scope.today = $filter("date")(Date.now(), 'yyyy-MM-dd');
+		this.nuevoMovimiento = {
+			esIngreso: 1, esGasto: 0, importe: 0,
+			//$scope.nuevoMovimiento.fecha = $filter("date")(Date.now(), 'yyyy-MM-dd');
+			fecha: new Date().toJSON().split('T')[0]
+		};
+		
+		this.total = { ingresos: 0, gastos: 0 };
+		
+		this.movimientos = [];
+		
+		this.maestros = {
+		  categoriasIngresos : ['Nomina', 'Ventas', 'Intereses depositos'],
+		  categoriasGastos : ['Hipoteca', 'Compras', 'Impuestos']
+		};
+
+		this.saveMovimiento = function(){
+			var newMov = this.nuevoMovimiento; //reutilizamos codigo
+
+			// this.total.ingresos += newMov.esIngreso * newMov.importe;
+			// this.total.gastos += newMov.esGasto * newMov.importe;
+			( !newMov.esIngreso ) 	? this.total.gastos += newMov.importe 
+											: this.total.ingresos += newMov.importe;
+
+			this.movimientos.push({
+				fecha: newMov.fecha,
+				tipo: this.tipoMovimiento(),
+				categoria: newMov.categoria,
+				importe: newMov.importe
+			});
+			newMov.importe = 0;
+		};
+
+		this.tipoMovimiento = function(){
+			var tipoMov = (!this.nuevoMovimiento.esIngreso) ? 'gasto' : 'ingreso';
+			return tipoMov;		
+		};
+
+		this.balance = function(){
+			return this.total.ingresos - this.total.gastos;
+		};
 	});
 
 })(window, window.angular);
