02cae8d3d24e276f848c8a18d02d8688d247df10
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-07-18 19:40:21 +0200

    implementar controlador de registro, login. consumir API REST

--- client/js/register.ctrl.js
+++ client/js/register.ctrl.js
@@ -1,6 +1,30 @@
 (function(w, ng, angApp){
 	var registerCtrl = function($rootScope, $location, $http, $cookieStore){
+		var	urlBase = "http://localhost:3000/api/",
+				usuario = {};
 
+		// $rootScope permite usar las propieades fijadas a el en cualquier lugar de la aplicacion
+		this.login = function(){
+			var urlLogin = urlBase+"sesiones/";
+			$http.post(urlLogin, usuario)
+					.success(function(data){
+						$rootScope.nombre = this.usuario.email; 
+						$rootScope.mensaje = 'Acceso Correcto, sesion actualizada';
+						$cookieStore.put('sessionId', data);
+						$location.path("/"):
+					});
+		};
+		this.registro = function(){
+			var urlLogin = urlBase+"usuarios/";
+			$http.post(urlLogin, usuario)
+					.success(function(data){
+						$rootScope.nombre = this.usuario.email; 
+						$rootScope.mensaje = 'Es tu primera conexion al Servidor';
+						$cookieStore.put('sessionId', data);
+						$location.path("/"):
+					});
+		};
 	};
-	angApp.controller("loginRegisterController", ['$rootScope', '$location', '$http', '$cookieStore', registerCtrl]);
+	angApp.controller("loginRegisterController",
+							['$rootScope', '$location', '$http', '$cookieStore', registerCtrl]);
 }(window, window.angular, app));
