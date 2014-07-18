6cec432c78c64ecc7dd95601d8d860fd7ca5c5ee
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-07-18 19:17:38 +0200

    inicializar e implementar dependencias en controlador de vista-registro

---
+++ client/js/register.ctrl.js
@@ -0,0 +1,6 @@
+(function(w, ng, angApp){
+	var registerCtrl = function($rootScope, $location, $http, $cookieStore){
+
+	};
+	angApp.controller("loginRegisterController", ['$rootScope', '$location', '$http', '$cookieStore', registerCtrl]);
+}(window, window.angular, app));
