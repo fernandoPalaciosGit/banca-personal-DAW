1a3cc8ce37f7ba6373d011ed63159a3cb13dc083
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-07-20 19:00:53 +0200

    NODE servder, controlar desconexion, optimizar login / registro

--- server/httpServer.js
+++ server/httpServer.js
@@ -1,21 +1,48 @@
 var express = require('express'), //MVC
-    bodyParser = require('body-parser'); //HTTP req, resp
+    bodyParser = require('body-parser'),//HTTP req, resp
+    http = require('http'); //controlar eventos de express
 
-//aplicacion MVC basada en express
-var app = express();
-app.use( bodyParser() );
+var app = express(),            //aplicacion MVC basada en express
+    server = app.listen(3000);  //puerto de escucha del servidor (localhost)
 
-//PERSISTENCIA DE MOVIMIENTOS
-var	total = { ingresos: 0, gastos: 0 },
-	maestros = {
-		categoriasIngresos	: ['Nomina', 'Ventas', 'Intereses depositos'],
-		categoriasGastos	: ['Hipoteca', 'Compras', 'Impuestos']
-	},
-    movimientos = [];
-
-//AUTENTIFICACION
-var usuarios = [],
+    //PERSISTENCIA DE MOVIMIENTOS
+var movimientos = [],
+    total = { ingresos: 0, gastos: 0 },
+    maestros = {
+        categoriasIngresos  : ['Nomina', 'Ventas', 'Intereses depositos'],
+        categoriasGastos    : ['Hipoteca', 'Compras', 'Impuestos']
+    },
+    //AUTENTIFICACION
+    usuarios = [],
     sesiones = [];
+
+var newSession = function ( newEmailUser ){
+    //dar de alta con el token de sesion a traves del email
+    var newSessionId = (Math.random()*88888) + 11111,
+        resetTimeStamp = new Date();
+    
+    sesiones.push({
+        email: newEmailUser,
+        sessionId: newSessionId,
+        timeStamp: resetTimeStamp
+    });
+    return newSessionId;
+};
+
+var shutDown = function (){
+    console.log("el servidor se ha desconectado");
+
+    server.close(function(){
+        process.exit();
+    });
+
+    //a los 5 segundos forzaremos el apagado
+    setTimeout(function(){
+       process.exit(); 
+    }, 1000*5);
+};
+
+app.use( bodyParser() );
 
 //MIDDLEWARE, acceso a recursos estaticos desde este servidor
 app.use( express.static('../client') );
@@ -24,33 +51,25 @@ app.use( express.static('../client') );
 app.use( '/api/priv/', function (req, res, next){
     //APLICAMOS UNA REGLA PARA TODAS LAS URL QUE COMIENZEN POR /api/priv
     var sessionId = req.get('sessionId');
-    var sesionEncontrada = sesiones.filter(function(sesion){
-            return sesion.sessionID === sessionId;
+    //console.log("SESION: "+sessionId);
+    var sesionEncontrada = sesiones.filter(function (sesion){
+            //comparacion solo de valor: Number&&String
+            return sesion.sessionId == sessionId;
         })[0];
 
     //ACTUALIZAR LA SESION si no han pasado 20 minutos o CADUCARLA
     if(sesionEncontrada){
-        if( (new Date() - sesionEncontrada.timeStamp) > (1000*60*20) ){
+        if( (new Date() - sesionEncontrada.timeStamp) > (1000*20*60) ){
             console.log('Sesion caducada: '+JSON.stringify(sesionEncontrada));
             res.send(419, 'Sesion Caducada');
         }else{
             sesionEncontrada.timeStamp = new Date();
         }
     }else{
-        res.send(401, 'Credencial Invalida');
+        res.send(401, 'pasaron 20 minutos, vuelva a loguearse');
     }
     //NODE: si hemos llegado aqui, continuar la ejecucion
     next();
-});
-
-//API REST: prueba de servidor
-app.get("/test", function(req, res, next){
-	res.send("<h1>Flujo de Cajas</h1><p>NodeJs y Expres funcionan!!!</p>");
-});
-
-//API REST: recuperar maestros
-app.get("/api/pub/maestros", function(req, res, next){
-	res.json(maestros);
 });
 
 //API REST: recuperar totales
@@ -82,8 +101,13 @@ app.route('/api/priv/movimientos')
         res.status(200);
     });
 
+//API REST: recuperar maestros
+app.get("/api/pub/maestros", function(req, res, next){
+    res.json(maestros);
+});
+
 //API REST: gestion de Usuarios: lista y redistros
-app.route('/api/usuarios')
+app.route('/api/usuarios/')
     .get(function (req, res, next){
         res.json(usuarios);
     })
@@ -99,6 +123,7 @@ app.route('/api/usuarios')
             if( usuarios[i].email === checkUsuario.email){
                 console.log('Usuario con email ya registrado: '+checkUsuario.email);
                 res.send(409, "Email '"+checkUsuario.email+"' ya registrado");
+                return true;
             }
         }
 
@@ -107,19 +132,6 @@ app.route('/api/usuarios')
         var sessionID = newSession(checkUsuario.email);
         res.json(sessionID);
     });
-
-var newSession = function ( newEmailUser ){
-    //dar de alta con el token de sesion a traves del email
-    var sessionID = Math.random() * (88888) + 11111,
-        timeStamp = new Date();
-    
-    sesiones.push({
-        email: newEmailUser,
-        sessionID: sessionID,
-        timeStamp: timeStamp
-    });
-    return sessionID;
-};
 
 // API REST: gestion de sesiones
 /*si el usuario esta registrado, comprobar si la sesionque envia el cliente esta dada de alta para ese cliente en el servidor (email)*/
@@ -133,19 +145,23 @@ app.route('/api/sesiones')
             email: reqBody.email,
             password: reqBody.password
         };
-        
         //confirmar usuario o negar conexion
         for (var i = 0, len = usuarios.length; i < len; i++) {
             if( usuarios[i].email === checkUsuario.email &&
                 usuarios[i].password === checkUsuario.password    ){
                 var sessionID = newSession(checkUsuario.email);
                 res.json(sessionID);
+                return true;
             }
         }
-
         console.log("Credencial Invalida: "+checkUsuario.email);
         res.send(401, 'Credencial Invalida');
     });
 
-//puerto de escucha del servidor (localhost)
-app.listen(3000);
+//API REST: prueba de servidor
+app.get("/test", function(req, res, next){
+    res.send("<h1>Flujo de Cajas</h1><p>NodeJs y Expres funcionan!!!</p>");
+});
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
